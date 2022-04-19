d0f1faaf44c2ae4709f4480e521420576187c47f
Frankz <leninalarcon7@gmail.com>
2022-04-19 16:50:34 -0500

    refactor

--- src/db/config.js
+++ src/db/config.js
@@ -1,17 +1,12 @@
 const config = require('../config');
 
-const DB_USER = encodeURIComponent(config.dbUser);
-const DB_PASSWORD = encodeURIComponent(config.dbPassword);
-
-const URI = `postgres://${DB_USER}:${DB_PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-
 module.exports = {
   development: {
-    url: URI,
+    url: config.dbUri,
     dialect: 'postgres',
   },
   production: {
-    url: URI,
+    url: config.dbUri,
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
